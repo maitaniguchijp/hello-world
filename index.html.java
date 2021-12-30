0949ee0f36c7b4aeaca36eaf9bacaeb47015ee61
mai <96604452+maitaniguchijp@users.noreply.github.com>
2021-12-30 09:47:55 +0900

    Add files via upload

---
+++ index.html.java
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<body>
+<h1>AddressForm</h1>
+<div class="form-group">  
+<input type="street" 
+         class="form-control" 
+         id="autocomplete" 
+         placeholder="Street">
+  
+  <input type="city" 
+         class="form-control" 
+         id="inputCity" 
+         placeholder="City">
+  
+  <input type="state" 
+         class="form-control" 
+         id="inputState" 
+         placeholder="State">
+  
+  <input type="zip" 
+         class="form-control" 
+         id="inputZip" 
+         placeholder="Zip">
+  
+  <input type="county" 
+         class="form-control" 
+         id="inputCounty" 
+         placeholder="County">
+  
+
+<image src="./addressimage1.png.png" height="50">
+</div>
+
+
+</body>
+</html>
